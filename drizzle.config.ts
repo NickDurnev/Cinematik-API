7d018e234ec8d0e03e239e9b0468ec2d82e42f44
Nick <nikitaspec@gmail.com>
2025-09-20 18:06:11 +0300

    feat: add SSL configuration for database connections, deploy ci-cd yml file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,5 +9,9 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: true }
+        : { rejectUnauthorized: false },
   },
 });
