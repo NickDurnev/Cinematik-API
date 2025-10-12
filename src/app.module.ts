3e697074cfb5e1882a9e6cf31f0e28a8d5e19e23
Nick <nikitaspec@gmail.com>
2025-10-12 22:20:47 +0300

    chore: add i18n support with nestjs-i18n and update package version to 0.1.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
+import { join } from "path";
+
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { HeaderResolver, I18nModule } from "nestjs-i18n";
 
 import { AppController } from "@/app.controller";
 import AuthModule from "@/auth/auth.module";
@@ -9,7 +12,6 @@ import MoviesModule from "@/movies/movies.module";
 import ReviewsModule from "@/reviews/reviews.module";
 
 import ProfileModule from "./profile/profile.module";
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,6 +19,17 @@ import ProfileModule from "./profile/profile.module";
       validate: config => {
         return configValidationSchema.parse(config);
       },
+    }),
+    I18nModule.forRootAsync({
+      useFactory: () => ({
+        fallbackLanguage: "en",
+        loaderOptions: {
+          path: join(__dirname, "/i18n/"),
+          watch: true,
+        },
+        typesOutputPath: join(__dirname, "../src/generated/i18n.generated.ts"),
+      }),
+      resolvers: [new HeaderResolver(["x-custom-lang"])],
     }),
     ReviewsModule,
     MoviesModule,
